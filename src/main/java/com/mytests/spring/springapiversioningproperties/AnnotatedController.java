f9d2be01ea44d673db330d93575658d0d9fd229f
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-27 17:41:44 +0200

    version with prefix

--- src/main/java/com/mytests/spring/springapiversioningproperties/AnnotatedController.java
+++ src/main/java/com/mytests/spring/springapiversioningproperties/AnnotatedController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/annotated")
 public class AnnotatedController {
 
-	@GetMapping(path = "/test1/{id}", version = "1.1")
+	@GetMapping(path = "/test1/{id}", version = "version1.1")
 	public String getTest11WithPathVariable(@PathVariable String id) {
 		return "annotated test1 with path variable "+id+", version 1.1";
 	}
-	@GetMapping(path = "/test1/{id}", version = "1.2")
+	@GetMapping(path = "/test1/{id}", version = "1.2+")
 	public String getTest12WithPathVariable(@PathVariable String id) {
 		return "annotated test1 with path variable "+id+", version 1.2";
 	}
