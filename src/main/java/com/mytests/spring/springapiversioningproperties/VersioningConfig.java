be40c73cecabdff7e5e90caa110389edfc4ba87d
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-27 17:43:13 +0200

    configure media-type apiinserter via programmatic configuration

---
+++ src/main/java/com/mytests/spring/springapiversioningproperties/VersioningConfig.java
@@ -0,0 +1,22 @@
+package com.mytests.spring.springapiversioningproperties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class VersioningConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureApiVersioning(ApiVersionConfigurer configurer) {
+		configurer.useMediaTypeParameter(MediaType.APPLICATION_JSON, "mediatype-version");
+		//configurer.useRequestHeader("My-Version");
+		//configurer.useQueryParam("version-param");
+		configurer.setVersionRequired(false);
+		configurer.addSupportedVersions("1.1", "1.2");
+		configurer.detectSupportedVersions(false);
+	}
+
+
+}
