1b5605b1022cd4953e00653cd84564356e1c1492
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-30 22:12:31 +0200

    readme: the problem with application.yaml is fixed - add mark

--- src/main/java/com/mytests/spring/springapiversioningproperties/VersioningConfig.java
+++ src/main/java/com/mytests/spring/springapiversioningproperties/VersioningConfig.java
@@ -5,14 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class VersioningConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureApiVersioning(ApiVersionConfigurer configurer) {
 		configurer.useMediaTypeParameter(MediaType.APPLICATION_JSON, "mediatype-version");
 		//configurer.useRequestHeader("My-Version");
-		//configurer.useQueryParam("version-param");
+		configurer.useQueryParam("version-param");
 		configurer.setVersionRequired(false);
 		configurer.addSupportedVersions("1.1", "1.2");
 		configurer.detectSupportedVersions(false);
